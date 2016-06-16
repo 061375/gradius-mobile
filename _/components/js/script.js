716cc9331cb22893ec748db3d124e38c0c52f3cf
root <root@localhost.localdomain>
2016-06-16 01:56:48 -0400

    animate parralax bg

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,1 +1,12 @@
-//
+$(document).ready(function(){
+    $('#ins').on("click",function(e){
+        e.preventDefault();
+        $('.info .show').removeClass("show");
+        $('#_ins').addClass("show");
+    });
+    $('#abo').on("click",function(e){
+        e.preventDefault();
+        $('.info .show').removeClass("show");
+        $('#_abo').addClass("show");
+    });
+});
