9706fdcd03a6b35d84df58406846190b32b0ecca
Jeremy Heminger <jeremy@geomail.info>
2016-07-12 16:57:26 -0700

    documentation

--- _/components/js/enemies.js
+++ _/components/js/enemies.js
@@ -3,38 +3,29 @@ var ENEMIES = {
     c:0,
     test:true,
     loadmodel: function(name,callback) {
-            // currently this assumes that the model has only one texture
-            // other sprites should be loaded seperately
-            // will add a methid to append other textures if nexxessary
-            var mtlLoader = new THREE.MTLLoader();
-			mtlLoader.load( '_/models/'+name+'.mtl', function( materials ) {
-                materials.preload();
-                
-                var loader = new THREE.OBJLoader();
-                loader.setMaterials( materials );
-                loader.load( '_/models/'+name+'.obj', function ( object ) {
-                    callback(object); 
-                }, onProgress, onError );
-            });
+        // currently this assumes that the model has only one texture
+        // other sprites should be loaded seperately
+        // will add a methid to append other textures if nexxessary
+        var mtlLoader = new THREE.MTLLoader();
+                    mtlLoader.load( '_/models/'+name+'.mtl', function( materials ) {
+            materials.preload();
+            
+            var loader = new THREE.OBJLoader();
+            loader.setMaterials( materials );
+            loader.load( '_/models/'+name+'.obj', function ( object ) {
+                callback(object); 
+            }, onProgress, onError );
+        });
     },
+    /**
+     * @returns {Void}
+     * */
     loop: function() {
-        
-        if(this.e.length > 0 && this.test){
-            //console.log(this.e);
-            //this.test = false;
-            //this.e[1].loop();
+        for(var p in this.e) {
+            this.e[p].loop();
         }
-        //if(this.e.length > 0) {
-            var l = this.e.length;
-            /*
-            for(i = 1; i<l; i++) {
-                this.e[i].loop(i);
-            }*/
-            for(var p in this.e) {
-                this.e[p].loop();
-            }
-        //}
     },
+    // this should be added to a general class
     addDebugpathNodes:function(a_p) {
         var l = a_p.length;
         var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
@@ -53,39 +44,32 @@ var ENEMIES = {
      * @param {number} y target y
      * @param {variant} t can be the turn speed (number) or the string 'variant'
      * @param {number} m roll max ( if roll max < 0 then this roll min)
+     * @returns {Object}
      * */
     turn: function(obj,x,y,t,m) {
         var d = point_direction(obj.position.x,obj.position.y,x,y,false);
-        
-        //obj.rotation.z = Math.radians(d);
 
         // if 'variant' then set the rotation speed based on location of enemy
         if(t == 'variant') t = Math.abs(Math.abs(x) - d);
 
         var r = Math.degrees(obj.rotation.z);
-        
-        //console.log(' r:'+r);
-        //console.log(' d > '+d);
-        //console.log(' t:'+t);
+
         if (r >= 180 && (360 - d) > 180) {
-            if (d > (r-10)){//console.log(d+' > '+r);
+            if (d > (r-10)){
             r-=t;}
-            if (d < (r+10)){//console.log(d+' < '+r);
+            if (d < (r+10)){
             r+=t;}
         }else{
-            if (d > (r-10)){//console.log(d+' > '+r);
+            if (d > (r-10)){
             r+=t;}
-            if (d < (r+10)){//console.log(d+' < '+r);
+            if (d < (r+10)){
             r-=t;}
         }
     
         if (r < 0) r=360;
         if (r > 360) r=0;
-       
-       //console.log(' r:'+r);
-       //console.log('------------------------------');
         obj.rotation.z = Math.radians(r);
-        //obj.rotation.z = Math.radians(d);
+
         //this.roll(obj,(t/10),m);
         
         return obj;
@@ -94,6 +78,7 @@ var ENEMIES = {
      * @param {object} obj the enemy object
      * @param {number} s roll speed
      * @param {number} m roll max ( if roll max < 0 then this roll min)
+     * @returns {Object}
      * */
     roll: function(obj,s,m) {
         // jeremy this is psuedo code !!!
@@ -105,6 +90,12 @@ var ENEMIES = {
         obj.rotation.y = r;
         return obj;
     },
+    /**
+     * Removes the enemy from the scene and its referance in the hierarchy
+     * NOTE: I may add this to a more global class...or maybe i'll do that if i make a game engine
+     * @param {string} me the name of the enemy to kill
+     * @returns {Void}
+     * */
     remove: function(me) {
         var selectedObject = scene.getObjectByName(me);
         scene.remove( selectedObject );
